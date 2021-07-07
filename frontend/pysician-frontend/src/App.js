d59047bcee5adc6c1ab13559cf05f02dcfb33871
Aksh4y2604 <63839864+Aksh4y2604@users.noreply.github.com>
2021-07-07 23:04:56 +0530

    added the login page

---
+++ frontend/pysician-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import SignIn from './components/login page/login'
+
+function App () {
+  return (
+    <div className='App'>
+      <SignIn></SignIn>
+    </div>
+  )
+}
+
+export default App
